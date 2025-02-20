ad35d82bfc59ab97b93f46f9ec8ac79093911038
Hirwavsopps <c.hirwa1@alustudent.com>
2025-02-20 15:14:38 -0800

    Initial commit

---
+++ scripts/reminder.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Script to remind students about upcoming deadlines
+source /submission_reminder_Hirwa/scripts/functions.sh
+check_deadlines
---
+++ scripts/startup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Script to start the reminder app
+echo "Starting submission reminder app..."
+/submission_reminder_Hirwa/scripts/reminder.sh
